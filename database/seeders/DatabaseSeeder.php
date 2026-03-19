fa831ca3872fc0643f802c493288b21f3e69c4be
Farid Islamudin <hi.faridudin@gmail.com>
2026-03-19 23:02:22 +0700

    feat: add role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // role seeder
+        $this->call(RoleSeeder::class);
     }
 }
