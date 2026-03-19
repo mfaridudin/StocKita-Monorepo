d1778cd0bf503d7a3cd9700bc3124af9c37bacf2
Farid Islamudin <hi.faridudin@gmail.com>
2026-03-19 22:06:59 +0700

    feat: auth with google

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback()
+    {
+        // $googleUser = Socialite::driver('google')->user();
+
+        $googleUser = Socialite::driver('google')->stateless()->user();
+
+        $user = User::where('email', $googleUser->email)->first();
+
+        if (! $user) {
+            $user = User::create([
+                'name' => $googleUser->name,
+                'email' => $googleUser->email,
+                'email_verified_at' => now(),
+                'password' => bcrypt('password123'),
+                'role' => 'owner',
+            ]);
+        }
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -9,7 +9,9 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\GoogleController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -33,6 +35,10 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
+
+    // google auth
+    Route::get('/auth/google', [GoogleController::class, 'redirect']);
+    Route::get('/auth/google/callback', [GoogleController::class, 'callback']);
 });
 
 Route::middleware('auth')->group(function () {
