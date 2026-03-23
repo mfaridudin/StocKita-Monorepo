e9b4236204b91676d35f6d4749939714d6a57e33
Farid Islamudin <hi.faridudin@gmail.com>
2026-03-23 18:00:43 +0700

    feat: add detail product page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -75,7 +75,9 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+
+        return view('produk.show', compact('product'));
     }
 
     /**
