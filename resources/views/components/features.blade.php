05197dac5f972425852d6f1a6d35f488e53ef54e
Farid Islamudin <hi.faridudin@gmail.com>
2026-04-01 11:26:32 +0700

    feat: add features

---
+++ resources/views/components/features.blade.php
@@ -0,0 +1,87 @@
+<section id="features" class="py-24 bg-gradient-to-b from-white to-emerald-50">
+    <div class="max-w-7xl mx-auto">
+
+        <div class="text-center mb-16">
+            <h2 class="text-4xl font-bold text-slate-800 mb-4">
+                Fitur Unggulan
+            </h2>
+            <p class="text-slate-600">
+                Semua yang kamu butuhkan untuk mengelola toko dengan mudah
+            </p>
+        </div>
+
+        <div class="grid md:grid-cols-3 gap-8">
+            <div class="feature-item bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden">
+                <img src="https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?w=800"
+                    class="h-40 w-full object-cover">
+
+                <div class="p-5">
+                    <h3 class="font-semibold text-lg mb-2">POS Modern & Cepat</h3>
+                    <p class="text-sm text-gray-600">
+                        Sistem kasir real-time untuk transaksi cepat, efisien, dan minim kesalahan.
+                    </p>
+                </div>
+            </div>
+
+            <div class="feature-item bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden">
+                <img src="https://images.unsplash.com/photo-1586528116311-ad8dd3c8310d?w=800"
+                    class="h-40 w-full object-cover">
+
+                <div class="p-5">
+                    <h3 class="font-semibold text-lg mb-2">Manajemen Stok Pintar</h3>
+                    <p class="text-sm text-gray-600">
+                        Pantau stok barang secara otomatis dengan indikator produk hampir habis.
+                    </p>
+                </div>
+            </div>
+
+            <div class="feature-item bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden">
+                <img src="https://images.unsplash.com/photo-1654263736203-a289f57c0d82?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    class="h-40 w-full object-cover">
+
+                <div class="p-5">
+                    <h3 class="font-semibold text-lg mb-2">Struk Otomatis & Profesional</h3>
+                    <p class="text-sm text-gray-600">
+                        Generate struk transaksi secara otomatis dengan tampilan rapi dan siap cetak.
+                    </p>
+                </div>
+            </div>
+
+            <div class="feature-item bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden">
+                <img src="https://plus.unsplash.com/premium_photo-1661297441050-cd5f9980051d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8TGFwb3JhbiUyMCUyNiUyMFN0YXRpc3Rpa3xlbnwwfHwwfHx8MA%3D%3D"
+                    class="h-40 w-full object-cover">
+
+                <div class="p-5">
+                    <h3 class="font-semibold text-lg mb-2">Laporan & Statistik</h3>
+                    <p class="text-sm text-gray-600">
+                        Analisis penjualan dan performa toko dengan dashboard statistik interaktif.
+                    </p>
+                </div>
+            </div>
+
+            <div class="feature-item bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden">
+                <img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=800"
+                    class="h-40 w-full object-cover">
+
+                <div class="p-5">
+                    <h3 class="font-semibold text-lg mb-2">Manajemen Pelanggan</h3>
+                    <p class="text-sm text-gray-600">
+                        Kelola data pelanggan dan kirim email promosi atau notifikasi dengan mudah.
+                    </p>
+                </div>
+            </div>
+
+            <div class="feature-item bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden">
+                <img src="https://plus.unsplash.com/premium_photo-1733328013343-e5ee77acaf05?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fE11bHRpJTIwUm9sZSUyMCUyNiUyMEFrc2VzfGVufDB8fDB8fHww"
+                    class="h-40 w-full object-cover">
+
+                <div class="p-5">
+                    <h3 class="font-semibold text-lg mb-2">Multi Role & Akses</h3>
+                    <p class="text-sm text-gray-600">
+                        Sistem role Admin, Owner, dan Buyer untuk kontrol penuh dan aman.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
