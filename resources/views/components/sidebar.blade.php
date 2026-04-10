5769d6a51740100d30974e0cb24ae63b6a3ccf41
Farid Islamudin <hi.faridudin@gmail.com>
2026-04-10 11:29:32 +0700

    fix: sidebar roles admin

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -161,7 +161,7 @@
         <div class="border-t border-green-100 my-4"></div>
 
         @role('admin')
-            <a href="/roles" class="nav-item {{ str_starts_with($currentPath, 'roles') ? 'active' : '' }}">
+            <a href="/admin/roles" class="nav-item {{ str_starts_with($currentPath, 'admin/roles') ? 'active' : '' }}">
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z">
