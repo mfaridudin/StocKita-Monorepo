106e5a87772b7a3c1559b3eb941dbabf3e287d8f
Farid Islamudin <hi.faridudin@gmail.com>
2026-04-13 09:48:17 +0700

    feat: add profile permission

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,6 +11,11 @@ use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('permission: edit own profile');
+    }
+
     /**
      * Display the user's profile form.
      */
