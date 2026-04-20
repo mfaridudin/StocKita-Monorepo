1e0c7438a254b99f6fe054757810bd75367118d9
Farid Islamudin <hi.faridudin@gmail.com>
2026-04-20 10:41:34 +0700

    fix: vite js detail features & blogs

--- resources/views/blogs/detail.blade.php
+++ resources/views/blogs/detail.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
 
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-gray-50 text-gray-800">
